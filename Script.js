ade45531282ce5a6509b02ed04b9297647d6de09
Rajveer <khubanirajdeep2@gmail.com>
2026-08-01 16:15:53 +0530

    Add password check functionality in Script.js
    Implement password check and error handling for access.

--- Script.js
+++ Script.js
@@ -1,1 +1,43 @@
+// Friendship Vault Password
 
+function checkPassword() {
+
+    const password = document.getElementById("password").value;
+
+    const error = document.getElementById("error");
+
+    if(password === "RoKo"){
+
+        error.style.color="#00ff88";
+
+        error.innerHTML="✅ Access Granted...";
+
+        setTimeout(function(){
+
+            window.location.href="choose.html";
+
+        },1200);
+
+    }
+
+    else{
+
+        error.style.color="#ff4d4d";
+
+        error.innerHTML="❌ Wrong Password";
+
+    }
+
+}
+
+// Press Enter to Unlock
+
+document.addEventListener("keydown",function(e){
+
+    if(e.key==="Enter"){
+
+        checkPassword();
+
+    }
+
+});
